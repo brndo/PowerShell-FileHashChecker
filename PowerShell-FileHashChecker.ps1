c6571c98c08e320706d1c5896bd94e0f681eb485
brndo <brander.patrick@gmail.com>
2022-09-07 09:45:47 -0400

    Added logic to compare computed hash against provided hash

--- PowerShell-FileHashChecker.ps1
+++ PowerShell-FileHashChecker.ps1
@@ -1,20 +1,39 @@
-Function Check-FileHash {
-[CmdletBinding()]
-param (
-    [Parameter(Mandatory=$true)]
-    [string]$FilePath,
-    [string]$Algorithm,
-    [string]$Hash
-)
-
-## Test to see if path exists first
-if (Test-Path -Path $FilePath){
-        $ComputedHash = Get-FileHash -Path $FilePath -Algorithm $Algorithm
-
+Function Compare-FileHash {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$FilePath,
+        [string]$Algorithm,
+        [string]$Hash
+    )
+    
+    ## Test to see if path exists first
+    if (Test-Path -Path $FilePath){
+            $HashResults = Get-FileHash -Path $FilePath -Algorithm $Algorithm
+            $ComputedHash = $HashResults.Hash
+    
+            if ($ComputedHash -eq $Hash){
+                Write-Host "
+                Hashes match: 
+                Computed Hash
+                $ComputedHash
+                Provided Hash:
+                $Hash
+                "
+            } else {
+                Write-Error "
+                Hashes Don't Match: 
+                Computed Hash
+                $ComputedHash
+                Provided Hash:
+                $Hash
+                "
+            }
+    
+        }
+    ## If Path does not exist, throw error    
+    else {
+        Write-Error "Cannot find file: $FilePath"
     }
-## If Path does not exist, throw error    
-else {
-    Write-Error "Cannot find file: $FilePath"
-}
-
+    
 }
